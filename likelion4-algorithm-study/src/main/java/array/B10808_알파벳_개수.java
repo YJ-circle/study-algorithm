0fbd6df5b653de1823c6d05ea73bae14f2f12a2e
thelightway <dev@thelightway.kr>
2025-06-01 11:52:08 +0900

    불필요한 내용 삭제

--- likelion4-algorithm-study/src/main/java/array/B10808_알파벳_개수.java
+++ likelion4-algorithm-study/src/main/java/array/B10808_알파벳_개수.java
@@ -1,9 +1,7 @@
 package array;
 
-import java.io.IOException;
-
 public class B10808_알파벳_개수 {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		int read;
 		int[] result = new int[26];
 		while ((read = System.in.read()) != 10 && read != 13) {
